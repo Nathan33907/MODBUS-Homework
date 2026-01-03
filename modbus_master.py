ab1887dc7363c2751249dc50a8b45a4ede53a959
nathandeng <nathan.demon66@gmail.com>
2026-01-04 01:24:56 +0800

    修复主站中的bug

--- modbus_master.py
+++ modbus_master.py
@@ -17,11 +17,11 @@ class ModbusMaster:
     def build_frame(self, slave, func, addr, value):
         frame = bytearray([slave, func])
 
-        if func in (0x01, 0x03):  # 读
+        if func in (0x01, 0x03):
             frame += addr.to_bytes(2, 'big')
             frame += value.to_bytes(2, 'big')
 
-        elif func == 0x05:        # 写单线圈
+        elif func == 0x05:
             frame += addr.to_bytes(2, 'big')
             frame += (0xFF00 if value else 0x0000).to_bytes(2, 'big')
 
@@ -29,10 +29,17 @@ class ModbusMaster:
         frame += bytes([crc & 0xFF, crc >> 8])
         return bytes(frame)
 
-    def send_and_recv(self, frame: bytes) -> bytes:
+    def send_and_recv(self, frame: bytes):
+        # ===== 真实串口 Tx =====
         self.ser.write(frame)
+        tx = frame[:]     # 实际发送的数据
+
         time.sleep(0.05)
-        return self.ser.read(256)
+
+        # ===== 真实串口 Rx =====
+        rx = self.ser.read(256)
+
+        return tx, rx     # ⚠️ 一定是两个值
 
     def close(self):
         self.ser.close()
